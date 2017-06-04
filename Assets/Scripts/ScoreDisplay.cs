83fa59f1caaf457a6e1d46f06b4bb88b40b697f4
Dustin Milotte <dmilotte@gmail.com>
2017-06-04 12:55:37 -0400

    added remaining files

---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreDisplay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void FillRollCard (List <int> rolls){
+		rolls [-1] = 5;
+	}
+}
