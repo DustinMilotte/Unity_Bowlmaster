3bc18d2bb1e8a7e9cf670a72296629f97c1157c3
Dustin Milotte <dmilotte@gmail.com>
2017-06-05 13:39:43 -0400

    catching up

---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour {
+	public Text [] rollTexts, frameTexts;
+		
+		 void Start () {
+		rollTexts[0].text = "X";
+		frameTexts[0].text = "0";
+	}
+	
+
+}
